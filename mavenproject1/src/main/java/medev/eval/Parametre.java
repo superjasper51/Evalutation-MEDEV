35401bcd09df6d2ffab400e73ba3006a3eda5c5d
superjasper51 <nath.degodet@gmail.com>
2026-01-12 16:43:43 +0100

    vérification de si le mot est valide repassée dans mot et ajout de la variabilité du nombre d'erreur

--- mavenproject1/src/main/java/medev/eval/Parametre.java
+++ mavenproject1/src/main/java/medev/eval/Parametre.java
@@ -7,6 +7,8 @@ package medev.eval;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import static java.lang.Integer.max;
+import static java.lang.Integer.min;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,20 +20,43 @@ public class Parametre {
 
     private List<String> dictionaire;
     private int nbErreurMax;
-
+    
+    /**
+     * constructeur par défaut de Parametre
+     */
     public Parametre() {
         this.dictionaire = new ArrayList<>(List.of("chat", "chien"));
         nbErreurMax = 7;
     }
 
+    /**
+     *
+     * @param mot qui ne doit contenir que des lettres
+     */
     public void ajouterMot(String mot) {
         if (!this.dictionaire.contains(mot)) {
-            this.dictionaire.add(mot);
+            boolean probleme = false;
+            int i = 0;
+            mot = mot.toLowerCase();
+            while (i < mot.length()) {
+                if (!ListeLettre.getAlphabet().contains(mot.charAt(i))) {
+                    System.out.println("Tous les mots du dictionaire doivent être valides, un mot ne l'est pas, il a été skip");
+                    probleme = true;
+                }
+            }
+            if (!probleme) {
+                this.dictionaire.add(mot);
+            }
+
         } else {
             System.out.println("ce mot existe déjà");//NO SONAR
         }
     }
 
+    /**
+     *
+     * @param mot qui doit faire partie du dictionaire
+     */
     public void supprimerMot(String mot) {
         if (this.dictionaire.contains(mot)) {
             this.dictionaire.remove(mot);
@@ -39,34 +64,33 @@ public class Parametre {
             System.out.println("ce mot n'existe pas dans la liste");//NO SONAR
         }
     }
+
     /**
-     * 
-     * @param fichier chemin d'acces du fichier
-     * le fichier ne doit contenir qu'un mot par ligne
+     *
+     * @param fichier chemin d'acces du fichier le fichier ne doit contenir
+     * qu'un mot par ligne
      */
-    public void chargerDictionnaire(String fichier){
+    public void chargerDictionnaire(String fichier) {
         try (BufferedReader br = new BufferedReader(new FileReader(fichier))) {
             String mot;
-            boolean probleme=false;
-            int i;
             while ((mot = br.readLine()) != null) {
-                i=0;
-                mot=mot.toLowerCase();
-                if (mot.contains(" ")){
-                System.out.println("il ne doit y avoir qu'un seul mot par ligne");//NO SONAR
-                }
-                while (i<mot.length()){
-                    if(!ListeLettre.getAlphabet().contains(mot.charAt(i))){
-                        System.out.println("Tous les mots du dictionaire doivent être valides, un mot ne l'est pas, il a été skip");
-                        probleme=true;
-                    }
-                }
-                if (!probleme){
+                if (mot.contains(" ")) {
+                    System.out.println("il ne doit y avoir qu'un seul mot par ligne");//NO SONAR
+                } else {
                     ajouterMot(mot);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     *
+     * @param n qui doit être compris entre 4 et 9, il y sera remis si il est
+     * hors de ce cadre
+     */
+    public void erreurMax(int n) {
+        this.nbErreurMax = min(9, max(4, n));
     }
 }
