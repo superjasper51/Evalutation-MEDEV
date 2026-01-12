a98126a1b89de15ff76a706f5a49e3feddcf4424
Julien Daudin <Julien.Daudin@eleves.ec-nantes.fr>
2026-01-12 17:50:18 +0100

    Nouveaux tests Mots

--- mavenproject1/src/test/java/medev/eval/MotTest.java
+++ mavenproject1/src/test/java/medev/eval/MotTest.java
@@ -19,11 +19,24 @@ public class MotTest {
     
     public MotTest() {
     }
-    @BeforeEach
+    @BeforeAll
     public void creation() {
         Mot mot=new Mot("aRbre");
         assert "ARBRE".equals(mot.getReel());
         assert "_ _ _ _ _ ".equals(mot.getActuel());
+    }
+    @BeforeAll
+    public void doubleLettre() {
+        Mot mot=new Mot("aRbre");
+        assert mot.possede('R');
+        assert mot.possede('r');
+    }
+    public void maj() {
+        Mot mot=new Mot("aRbre");
+        mot.majMotActuel('r',1);
+        assert "_ R _ _ _".equals(mot.getReel());
+        mot.majMotActuel('r',3);
+        assert "_ R _ R _".equals(mot.getReel());
     }
     
     @BeforeAll
