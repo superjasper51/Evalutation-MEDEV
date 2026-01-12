6dc6a3b5caa9d1f877aced38b8a30e46a3fa30e8
Julien Daudin <Julien.Daudin@eleves.ec-nantes.fr>
2026-01-12 15:17:15 +0100

    modif en liste et ce qu'il s'en suit

--- mavenproject1/src/main/java/medev/eval/Mot.java
+++ mavenproject1/src/main/java/medev/eval/Mot.java
@@ -4,18 +4,50 @@
  */
 package medev.eval;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author julda
  */
 public class Mot {
-    String reel;
-    String actuel;
+    private ArrayList<Character> reel;
+    private List<Character> actuel;
     public Mot(String mot){
-        reel=mot;
-        actuel="_";
-        for(int i=1; i<reel.length(); i++){
-            actuel=actuel+" _";
+        reel=new ArrayList<Character>();
+        actuel=new ArrayList<Character>();
+        for(int i=0; i<mot.length(); i++){
+            reel.add(mot.charAt(i));
+            actuel.add('_');
         }
+    }
+    
+    public void majMotActuel(char c, int place){
+        actuel.set(place,c);
+    }
+
+    public String getReel() {
+        String mot="";
+        for(Character a : reel){
+            mot=mot+a;
+        }
+        return mot;
+    }
+
+    public void setReel(ArrayList<Character> reel) {
+        this.reel = reel;
+    }
+
+    public void setActuel(ArrayList<Character> actuel) {
+        this.actuel = actuel;
+    }
+
+    public String getActuel() {
+        String mot="";
+        for(Character a : reel){
+            mot=mot+a+" ";
+        }
+        return mot;
     }
 }
